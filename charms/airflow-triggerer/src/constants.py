0cf1ecdb1039316bbc74dd8760c20191a66372e5
jananisenthilkumar291 <janani382001@gmail.com>
2026-01-22 17:40:05 -0500

    skip: cleanup accidental commit

--- charms/airflow-triggerer/src/constants.py
+++
@@ -1,6 +0,0 @@
-SERVICE_NAME = "airflow"
-CONTAINER_NAME = "airflow-triggerer"
-AIRFLOW_COMPONENT = "triggerer"
-AIRFLOW_COORDINATOR_RELATION_NAME = "airflow-coordinator"
-AIRFLOW_HOME = "/opt/airflow"
-AIRFLOW_CONFIG_PATH = f"{AIRFLOW_HOME}/airflow.cfg"
