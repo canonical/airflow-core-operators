ce20a064e5b388ce947a4d9a994bbdc9ba52c47d
jananisenthilkumar291 <janani382001@gmail.com>
2026-01-22 17:40:04 -0500

    Adding constants file

---
+++ airflow-api-server/src/api_server.py
@@ -0,0 +1,16 @@
+# Copyright 2025 Ubuntu
+# See LICENSE file for licensing details.
+
+"""Functions for interacting with the workload.
+
+The intention is that this module could be used outside the context of a charm.
+"""
+
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+def get_version() -> str | None:
+    """Get the running version of the workload."""
+    return None
---
+++ airflow-api-server/src/constants.py
@@ -0,0 +1,8 @@
+"""Shared constants for the Airflow API Server charm."""
+
+SERVICE_NAME = "airflow"
+CONTAINER_NAME = "airflow-api-server"
+AIRFLOW_COMPONENT = "api-server"
+AIRFLOW_COORDINATOR_RELATION_NAME = "airflow-coordinator"
+AIRFLOW_HOME = "/opt/airflow"
+AIRFLOW_CONFIG_PATH = f"{AIRFLOW_HOME}/airflow.cfg"
