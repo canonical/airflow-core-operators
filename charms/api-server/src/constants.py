34a4e72a278c8749a701dbae52283b8cb607ebf9
shayancanonical <99665202+shayancanonical@users.noreply.github.com>
2026-03-09 08:16:55 -0400

    [WF-288] Implement airflow api server relation charm lib + add provides interface (#23)
    * Implement airflow api server relation charm lib + add provides interface
    
    * Add charm lib docstring
    
    * Fix lint warnings
    
    * Update docstring for airflow-api-server charm lib methods
    
    * Update PR feedback; add docstrings for added tests
    
    * Add new relation between api-server and coordinatorin integration tests
    
    * Minor adjustments to further stabilize airflow funcitionality tests
    
    * Address PR feedback; add properties for api-server host and port values

--- charms/api-server/src/constants.py
+++ charms/api-server/src/constants.py
@@ -3,6 +3,9 @@
 SERVICE_NAME = "airflow"
 CONTAINER_NAME = "airflow-api-server"
 AIRFLOW_COMPONENT = "api-server"
-AIRFLOW_COORDINATOR_RELATION_NAME = "airflow-coordinator"
+
+AIRFLOW_COORDINATOR_RELATION_ENDPOINT = "airflow-coordinator"
+AIRFLOW_API_SERVER_RELATION_ENDPOINT = "airflow-api-server"
+
 AIRFLOW_HOME = "/opt/airflow"
 AIRFLOW_CONFIG_PATH = f"{AIRFLOW_HOME}/airflow.cfg"
